54f95def1be5f8ede9cf6a495272e19c560f407c
Anirudh Dutt <1468519+aneroid@users.noreply.github.com>
2022-12-01 11:57:55 +0530

    Day 01: as List of Strings with Builder

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,32 +1,38 @@
-class Day01(input: String) {
+class Day01(input: List<String>) {
     private val data = parseInput(input)
     
     fun partOne() =
         data.maxOf { it.sum() }
     
-    fun partTwo()=
+    fun partTwo() =
         data.map { it.sum() }
             .sortedDescending()
             .take(3)
             .sum()
     
     private companion object {
-        fun parseInput(input: String): List<List<Int>> =
-            input.split("\r\n\r\n", "\n\n")
-                .map { group ->
-                    group.split("\r\n", "\n")
-                        .filter { it.isNotBlank() }
-                        .map { it.toInt() }
+        fun parseInput(input: List<String>): List<List<Int>> =
+            buildList {
+                var group = mutableListOf<Int>()
+                for (line in input) {
+                    if (line.isBlank()) {
+                        add(group)
+                        group = mutableListOf()
+                    } else {
+                        group.add(line.toInt())
+                    }
                 }
+                add(group)
+            }
     }
 }
 
 fun main() {
-    val testInput = readInputAsString("Day01_test")
+    val testInput = readInput("Day01_test")
     check(Day01(testInput).partOne() == 24000)
     check(Day01(testInput).partTwo() == 45000)  // uncomment when ready
     
-    val input = readInputAsString("Day01")
+    val input = readInput("Day01")
     println("partOne: ${Day01(input).partOne()}\n")
     println("partTwo: ${Day01(input).partTwo()}\n")
 }
