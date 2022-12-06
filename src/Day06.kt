0c6e351cf3ff756887c134803029cadb5a2f36a9
Anirudh Dutt <1468519+aneroid@users.noreply.github.com>
2022-12-06 13:11:40 +0530

    Day 06 ✔ refactored
    - refactor test inputs & expected results

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,4 @@
 class Day06(private val input: String) {
-    private val data = parseInput(input)
-    
     private fun String.indexForUniqSeqLen(size: Int) =
         asSequence()
             .windowed(size) { it.toSet().size }
@@ -10,32 +8,17 @@ class Day06(private val input: String) {
     fun partOne(): Int = input.indexForUniqSeqLen(4)
     
     fun partTwo(): Int = input.indexForUniqSeqLen(14)
-    
-    private companion object {
-        fun parseInput(input: String) = input
-    }
 }
 
 fun main() {
-    val testInputs = listOf(
-        // testString to (partOneResult to partTwoResult)
-        "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to (7 to 19),
-        "bvwbjplbgvbhsrlpgdmjqwftvncz" to (5 to 23),
-        "nppdvjthqldpwncqszvftbrmjlhg" to (6 to 23),
-        "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to (10 to 29),
-        "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to (11 to 26),
-    )
+    val testInputs = readInput("Day06_test")
     val input = readInput("Day06").first()
     
     println("part One:")
-    testInputs.forEach { testInput ->
-        assertThat(Day06(testInput.first).partOne()).isEqualTo(testInput.second.first)
-    }
+    assertThat(testInputs.map { Day06(it).partOne()}).isEqualTo(listOf(7, 5, 6, 10, 11))
     println("actual: ${Day06(input).partOne()}\n")
     
     println("part Two:")
-    testInputs.forEach { testInput ->
-        assertThat(Day06(testInput.first).partTwo()).isEqualTo(testInput.second.second)
-    }
+    assertThat(testInputs.map { Day06(it).partTwo()}).isEqualTo(listOf(19, 23, 23, 29, 26))
     println("actual: ${Day06(input).partTwo()}\n")  // uncomment when ready
 }
