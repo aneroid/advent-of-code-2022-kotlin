de0a276c2585f559b5c3b48941c5e38af7d64aad
Anirudh Dutt <1468519+aneroid@users.noreply.github.com>
2022-12-11 17:08:28 +0530

    Day 11 ✔
    that reducer function! 😱

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,4 +1,5 @@
 class Day11(input: List<String>) {
+    private var worryReducer = { it: Long -> it / 3 }
     private val monkeys: List<Monkey> = parseInput(input)
     
     private fun printMonkeyItems(round: Int = 0) {
@@ -6,48 +7,53 @@ class Day11(input: List<String>) {
             println("After round $round:")
         }
         monkeys.forEachIndexed { index, monkey ->
-            println("Monkey $index: ${monkey.itemLevels}")
+            println("Monkey $index: ${monkey.itemLevels} | ${monkey.inspected}")
         }
         println()
     }
     
     private fun doRound() {
-        monkeys.forEachIndexed { index, monkey ->
+        monkeys.forEach { monkey ->
             monkey.see().map { monkey.`do`(it) }
         }
     }
     
     private fun Monkey.see() =
-        buildList {
+        buildList<Long> {
             while (itemLevels.isNotEmpty()) {
                 inspected++
-                itemLevels.removeFirst().let(changeLevel).div(3).also(::add)
+                itemLevels.removeFirst().let(changeLevel).let(worryReducer).also(::add)
             }
         }
     
-    private fun Monkey.`do`(level: Int) {
-        val throwTo = if (level % testDivBy == 0) throwTrue else throwFalse
+    private fun Monkey.`do`(level: Long) {
+        val throwTo = if (level % testDivBy == 0L) throwTrue else throwFalse
         monkeys[throwTo].itemLevels.addLast(level)
     }
     
-    fun partOne(): Int {
+    private fun solve(repetitions: Int): Long {
         printMonkeyItems()
-        repeat(20) {
+        repeat(repetitions) {
             doRound()
             // printMonkeyItems(it + 1)
         }
         printMonkeyItems()
-        return monkeys.map(Monkey::inspected).sortedDescending().take(2).reduce(Int::times)
+        return monkeys.map(Monkey::inspected).sortedDescending().take(2).map(Int::toLong).reduce(Long::times)
     }
     
-    fun partTwo(): Int {
-        return 0
+    fun partOne(): Long = solve(20)
+    
+    fun partTwo(repetitions: Int): Long {
+        val factor = monkeys.map(Monkey::testDivBy).also { println(it) }.reduce(Int::times)
+        println("Factor: $factor")
+        worryReducer = { it % factor }
+        return solve(repetitions)
     }
     
     private companion object {
         class Monkey(
-                val itemLevels: ArrayDeque<Int>,
-                val changeLevel: (Int) -> Int,
+                val itemLevels: ArrayDeque<Long>,
+                val changeLevel: (Long) -> Long,
                 val testDivBy: Int,
                 val throwTrue: Int,
                 val throwFalse: Int,
@@ -58,7 +64,7 @@ class Day11(input: List<String>) {
         fun parseInput(input: List<String>) =
             input.chunked(7).map { lines ->
                 Monkey(
-                    lines[1].substringAfter("items: ").split(", ").map(String::toInt).let(::ArrayDeque),
+                    lines[1].substringAfter("items: ").split(", ").map(String::toLong).let(::ArrayDeque),
                     lines[2].substringAfter("= ").toOperation(),
                     lines[3].substringAfter("by ").toInt(),
                     lines[4].substringAfter("monkey ").toInt(),
@@ -66,17 +72,17 @@ class Day11(input: List<String>) {
                 )
             }
         
-        fun String.toOperation(): (Int) -> Int {
-            val arg1 = substringBefore(" ").toIntOrNull()
-            val arg2 = substringAfterLast(" ").toIntOrNull()
-            val operator: (Int, Int) -> Int = when (substringAfter(" ").substringBefore(" ")) {
-                "+" -> Int::plus
-                "-" -> Int::minus
-                "*" -> Int::times
-                "/" -> Int::div
+        fun String.toOperation(): (Long) -> Long {
+            val arg1 = substringBefore(" ").toLongOrNull()
+            val arg2 = substringAfterLast(" ").toLongOrNull()
+            val operator: (Long, Long) -> Long = when (substringAfter(" ").substringBefore(" ")) {
+                "+" -> Long::plus
+                "-" -> Long::minus
+                "*" -> Long::times
+                "/" -> Long::div
                 else -> throw IllegalArgumentException("Unknown operation: $this")
             }
-            return { old: Int -> operator(arg1 ?: old, arg2 ?: old) }
+            return { old: Long -> operator(arg1 ?: old, arg2 ?: old) }
         }
     }
 }
@@ -90,6 +96,9 @@ fun main() {
     println("actual: ${Day11(input).partOne()}\n")
     
     println("part Two:")
-    // assertThat(Day11(testInput).partTwo()).isEqualTo(1)
-    // println("actual: ${Day11(input).partTwo()}\n")
+    assertThat(Day11(testInput).partTwo(20)).isEqualTo(99 * 103)
+    assertThat(Day11(testInput).partTwo(1_000)).isEqualTo(5204 * 5192)
+    assertThat(Day11(testInput).partTwo(2_000)).isEqualTo(10419 * 10391)
+    assertThat(Day11(testInput).partTwo(10_000)).isEqualTo(52166 * 52013L)
+    println("actual: ${Day11(input).partTwo(10_000)}\n")
 }
